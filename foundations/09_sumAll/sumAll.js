8374d4774aeee0c98acc90f376a628339fc83459
Jolly_Bones <samuelaphillips@colorado.edu>
2026-06-13 16:27:53 -0700

    Finish sumAll function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    numArray = [a, b]; 
+    numArray.sort((a,b) => a - b);
 
+    sum = 0;
+    for (i = numArray[0]; i <= numArray[1]; ++i)
+    {
+       sum += i; 
+    }
+    return sum;
 };
 
 // Do not edit below this line
